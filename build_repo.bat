75d43e1d4686a83317381585c5d6da8b6002bdc5
infoluka02 <infoluka02@gmail.com>
2026-07-03 14:10:20 +0700

    update repo

--- build_repo.bat
+++ build_repo.bat
@@ -1,5 +1,14 @@
 @echo off
-echo Đang build lại repository...
-python create_repository.py --datadir="./" "./addons/a4kSubtitles-repository" "./addons/plugin.program.openwizard" "./addons/repository.jellyfin.kodi" "./addons/skin.arctic.zephyr.mod-master"
-echo Build xong! Đang chờ sếp Push lên GitHub...
+setlocal enabledelayedexpansion
+
+echo Dang quet cac thu muc trong addons...
+set "ADDON_LIST="
+for /D %%i in (addons\*) do (
+    set "ADDON_LIST=!ADDON_LIST! "%%i""
+)
+
+echo Dang build lai repository...
+python create_repository.py --datadir="./" !ADDON_LIST!
+
+echo Build xong! Dang cho sep Push len GitHub...
 pause
